e96de607d1559f150ec8ec403048bc26a39a3c9a
ExErorr <kukuryk098@gmail.com>
2025-05-11 17:46:55 +0200

    up

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/React-Online-Shop/",
+  base: "/react-online-store/",
   plugins: [react()],
 });
